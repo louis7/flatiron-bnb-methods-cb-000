d8200d1c64453c32f1f52ef39bfb71c62b6b0f5c
louis baptiste <louisvis@ymail.com>
2019-08-23 00:25:48 +0000

    Automatically backed up by Learn

---
+++ spec/spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe CategoriesController, type: :controller do
+  describe "GET #show" do
+    it "returns HTTP success" do
+      get :show, id: @category
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns HTTP success" do
+      get :edit, id: @category
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
